13e377a0d7e555a5caf2a46825480d91edce861c
Mishkat Mustafid <mishkatmustafid@gmail.com>
2023-03-26 23:48:36 +0600

    Reorganized

---
+++ src/pages/api/home/index.tsx
@@ -0,0 +1,15 @@
+import Header from "../../../components/header";
+import Footer from "../../../components/footer";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Welcome to my website!</h1>
+        <p>This is the home page.</p>
+      </main>
+      <Footer />
+    </>
+  );
+}
