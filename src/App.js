77778b7737396f0a06dd2412f2c56312041083b5
Emmanuel Koduah Tweneboah <emmanueltweneboah71@gmail.com>
2023-08-04 13:57:37 +0000

    redux setup

--- src/App.js
+++ src/App.js
@@ -8,23 +8,27 @@ import StockPage from "./views/portfolioPage";
 import MarketOverview from "./views/marketOverview";
 import LeaderBoard from "./views/leaderboardPage";
 import Orders from "./views/Orders";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} exact />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<SignupPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/portfolio" element={<StockPage />} />
-          <Route path="/marketoverview" element={<MarketOverview />} />
-          <Route path="/leaderboard" element={<LeaderBoard />} />
-          <Route path="/transactions" element={<Orders />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />} exact />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<SignupPage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/portfolio" element={<StockPage />} />
+            <Route path="/marketoverview" element={<MarketOverview />} />
+            <Route path="/leaderboard" element={<LeaderBoard />} />
+            <Route path="/transactions" element={<Orders />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </Provider>
   );
 }
 
