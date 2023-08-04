a690f0274b0790cabccad77269e92c51c07e8794
Emmanuel Koduah Tweneboah <emmanueltweneboah71@gmail.com>
2023-08-04 11:34:11 +0000

    change css file names to index

--- src/views/auth/signupPage/index.jsx
+++ src/views/auth/signupPage/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./SignupPage.css";
+import "./index.css";
 import SpinnerLoader from "../../../components/spinner";
 
 const SignupPage = () => {
