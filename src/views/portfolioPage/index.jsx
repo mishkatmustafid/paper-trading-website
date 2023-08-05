184365969d66c34d5f35de22860988519aa07411
Emmanuel Koduah Tweneboah <emmanueltweneboah71@gmail.com>
2023-08-05 16:39:04 +0000

    add portfolio functionality

--- src/views/portfolioPage/index.jsx
+++ src/views/portfolioPage/index.jsx
@@ -9,17 +9,23 @@ import Modal from "react-modal";
 import WithAuth from "../../components/withAuth";
 
 const PortfolioPage = () => {
-  const [name, settName] = useState("");
+  const [name, setName] = useState("");
   const [selectedStock, setSelectedStock] = useState("google");
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedStockPrice, setSelectedStockPrice] = useState(0);
+  const [portfolios, setPortfolios] = useState([
+    {
+      name: "Portfolio 1",
+      dummyData: [
+        { label: "AMZ", stock: "amazon", quantity: 10, price: 1500 },
+        { label: "GOOGL", stock: "google", quantity: 20, price: 2500 },
+        { label: "MS", stock: "microsoft", quantity: 15, price: 1800 },
+      ],
+    },
+  ]);
 
   const openModal = (stock) => {
-    console.log(stock);
-    const foundStock = CurrentMarketData.find(
-      (item) => item.code === stock.stock
-    );
-    console.log("found", foundStock);
+    const foundStock = CurrentMarketData.find((item) => item.code === stock.stock);
     if (foundStock) {
       setSelectedStock(foundStock.code);
       setSelectedStockPrice(foundStock.priceNetVariation);
@@ -33,14 +39,22 @@ const PortfolioPage = () => {
     setSelectedStock("google");
   };
 
-  const dummyData = [
-    { label: "AMZ", stock: "amazon", quantity: 10, price: 1500 },
-    { label: "GOOGL", stock: "google", quantity: 20, price: 2500 },
-    { label: "MS", stock: "microsoft", quantity: 15, price: 1800 },
-  ];
+  const addPortfolio = () => {
+    if (name) {
+      setPortfolios((prevPortfolios) => [
+        ...prevPortfolios,
+        {
+          name: name,
+          dummyData: [], // You can initialize this with the initial portfolio data
+        },
+      ]);
+      setName("");
+    }
+  };
+
   return (
     <div className="container-fluid">
-      <Navbar  />
+      <Navbar />
       <PageHeading title="Portfolio Overview" />
       <div className="container-fluid">
         <div className="p-3 align-items-center ">
@@ -49,165 +63,82 @@ const PortfolioPage = () => {
         <div className="pb-5">
           <input
             style={{ width: "500px", height: "50px" }}
-            onChange={(e) => settName(e.target.value)}
+            onChange={(e) => setName(e.target.value)}
             value={name}
-            className="form-control-lg  custom-input"
-            placeholder="enter name of new portfolio "
+            className="form-control-lg custom-input"
+            placeholder="enter name of new portfolio"
             id="name"
             type="text"
           />
           <button
             style={{ width: "100px", height: "50px" }}
-            //   onClick={handleClick}
-            className="btn btn-secondary   btn-lg m-2 "
+            onClick={addPortfolio}
+            className="btn btn-secondary btn-lg m-2"
           >
             add
           </button>
         </div>
-        <div className="container">
-          <div className="row mb-3 border">
-            <div className="p-4 col-md-8">
-              <h3 className="p-3">Portfolio 1</h3>
-              <table className="p-3 table border table-striped">
-                <thead>
-                  <tr>
-                    <th>Stock</th>
-                    <th>Quantity</th>
-                    <th>Price</th>
-                    <th></th>
-                    <th>Sell</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {dummyData.map((order, index) => (
-                    <tr key={index}>
-                      <td>{order.label}</td>
-                      <td>{order.quantity}</td>
-                      <td className="text-danger">{order.price}</td>
-                      <td className="text text-success  ">
-                        {" "}
-                        <span>↑</span>{" "}
-                        <span className="text text-primary">↓</span>{" "}
-                      </td>
-                      <td>
-                        <button
-                          onClick={() => openModal(order)}
-                          className="btn btn-secondary"
-                        >
-                          sell
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-            <div className="p-4 col-md-4">
-              <MyPieChart />
-            </div>
+        {isModalOpen && (
+          <div className="modal-wrapper">
+            <Modal
+              className="custom-modal"
+              isOpen={isModalOpen}
+              onRequestClose={closeModal}
+            >
+              <PurchaseForm
+                stock={selectedStock}
+                currentPrice={parseFloat(selectedStockPrice.substring(1))}
+                closeModal={closeModal}
+                buttonTitle={"sell"}
+              />
+            </Modal>
           </div>
+        )}
 
-          <div className="row border mb-3">
-            <div className="p-4 col-md-8">
-              <h3 className="p-3">Portfolio 2</h3>
-              <table className="p-3 table border table-striped">
-                <thead>
-                  <tr>
-                    <th>Stock</th>
-                    <th>Quantity</th>
-                    <th>Price</th>
-                    <th>Status</th>
-                    <th>Sell</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {dummyData.map((order, index) => (
-                    <tr key={index}>
-                      <td>{order.label}</td>
-                      <td>{order.quantity}</td>
-                      <td className="text-danger">{order.price}</td>
-                      <td className="text text-success  ">
-                        {" "}
-                        <span>↑</span>{" "}
-                        <span className="text text-primary">↓</span>{" "}
-                      </td>
-                      <td>
-                        <button
-                          onClick={() => openModal(order)}
-                          className="btn btn-secondary"
-                        >
-                          sell
-                        </button>
-                      </td>
+        {portfolios.map((portfolio, index) => (
+          <div key={index} className="container">
+            <div className="row mb-3 border">
+              <div className="p-4 col-md-8">
+                <h3 className="p-3">{portfolio.name}</h3>
+                <table className="p-3 table border table-striped">
+                  <thead>
+                    <tr>
+                      <th>Stock</th>
+                      <th>Quantity</th>
+                      <th>Price</th>
+                      <th></th>
+                      <th>Sell</th>
                     </tr>
-                  ))}
-                  {isModalOpen && (
-                    <div className="modal-wrapper">
-                      <Modal
-                        className="custom-modal"
-                        isOpen={isModalOpen}
-                        onRequestClose={closeModal}
-                      >
-                        <PurchaseForm
-                          stock={selectedStock}
-                          currentPrice={parseFloat(
-                            selectedStockPrice.substring(1)
-                          )}
-                          closeModal={closeModal}
-                          buttonTitle={"sell"}
-                        />
-                      </Modal>
-                    </div>
-                  )}
-                </tbody>
-              </table>
-            </div>
-            <div className="p-4 col-md-4">
-              <MyPieChart />
+                  </thead>
+                  <tbody>
+                    {portfolio.dummyData.map((order, orderIndex) => (
+                      <tr key={orderIndex}>
+                        <td>{order.label}</td>
+                        <td>{order.quantity}</td>
+                        <td className="text-danger">{order.price}</td>
+                        <td className="text text-success">
+                          <span>↑</span>{" "}
+                          <span className="text text-primary">↓</span>{" "}
+                        </td>
+                        <td>
+                          <button
+                            onClick={() => openModal(order)}
+                            className="btn btn-secondary"
+                          >
+                            sell
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+              <div className="p-4 col-md-4">
+                <MyPieChart />
+              </div>
             </div>
           </div>
-          <div className="row border">
-            <div className="p-4 col-md-8">
-              <h3 className="p-3">Portfolio 3</h3>
-              <table className="p-3 table border table-striped">
-                <thead>
-                  <tr>
-                    <th>Stock</th>
-                    <th>Quantity</th>
-                    <th>Price</th>
-                    <th>Status</th>
-                    <th>Sell</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {dummyData.map((order, index) => (
-                    <tr key={index}>
-                      <td>{order.label}</td>
-                      <td>{order.quantity}</td>
-                      <td className="text-danger">{order.price}</td>
-                      <td className="text text-success  ">
-                        {" "}
-                        <span>↑</span>{" "}
-                        <span className="text text-primary">↓</span>{" "}
-                      </td>
-                      <td>
-                        <button
-                          onClick={() => openModal(order)}
-                          className="btn btn-secondary"
-                        >
-                          sell
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-            <div className="p-4 col-md-4">
-              <MyPieChart />
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
